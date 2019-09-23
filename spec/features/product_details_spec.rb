f3beb78802e389cdfcfdad90c0ade459ea197c75
John Webster <johnrwebs@gmail.com>
2019-09-23 21:56:04 +0000

    added capybara test for product details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", ype: :feature, js: true do
+  
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+      1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the details of one product" do
+    # ACT
+    visit root_path
+    click_link('Details')
+
+    # DEBUG / VERIFY
+    save_screenshot
+    expect(page).to have_css 'article.product', count: 1
+  end
+end
