3bbac866b31d2443df3d99005c06a274f42168ad
John Webster <johnrwebs@gmail.com>
2019-09-23 20:45:20 +0000

    adding rspec users model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :password, confirmation: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  subject { described_class.new(name: "Test", email: "test@test.com", password: "password", password_confirmation: "password") }
+  user2 = User.new(name: "Test2", email: "test2@test2.com", password: "password", password_confirmation: "password")
+
+  describe 'Validations' do
+      
+    it "should validate when fully filled out" do
+      expect(subject).to be_valid
+    end
+    it "should require a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Name can't be blank"
+    end
+    
+    #email validations
+    it "should require an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Email can't be blank"
+    end
+    #it "should error if the email already exists" do
+    #  subject.email = "test2@test2.com"
+    #  expect(subject).to_not be_valid
+    #  expect(subject.errors.full_messages.first).to eq "Email has already been taken"
+    #end
+    
+    #password validations
+    it "should require a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Password can't be blank"
+    end
+    it "should require password and password validation to match" do
+      subject.password_confirmation = "wrong"      
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eql "Password confirmation doesn't match Password"
+    end
+    it "should require a password length of at least 6" do
+      subject.password = "test"
+      subject.password_confirmation = "test"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eql "Password is too short (minimum is 6 characters)"
+    end
+  end
+end
